a9f3cef1841a3b5c8e5cbec613a21c2685177c11
shahid <shahidhashmiraj@gmail.com>
2024-05-30 16:08:21 +0530

    task manager completed

--- Task Manager/app.js
+++ Task Manager/app.js
@@ -5,11 +5,8 @@ const app = express();
 const route = require("./routes/routes");
 const PORT = 3000;
 
+app.use(express.static('./public'))
 app.use(express.json());
-
-app.get('/hello', (req,res)=>{
-    res.send("Task manager");
-});
 
 app.use('/api/v1/tasks', route);
 
